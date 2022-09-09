3bd59dcd72614ae65c66b28701c620c58ae769ef
valezzuniga <107826231+valezzuniga@users.noreply.github.com>
2022-09-09 09:25:25 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,92 @@
+// Dom7
+var $$ = Dom7;
+
+// Init App
+var app = new Framework7({
+  	id: 'com.app',
+  	root: '#app',
+	theme: 'md',
+	view: {
+        pushState: true
+    },
+	cache:false,
+	cacheDuration: 0,
+	modalTitle: 'Casa Di Vale App',
+  	routes: [
+		{
+		path: '/home/',
+    	url: 'index.html',
+    	name: 'home',
+  		},
+		{
+		path: '/menu/',
+    	url: 'menu.html',
+    	name: 'menu',
+  		},
+		{
+		path: '/galeria/',
+    	url: 'galeria.html',
+    	name: 'galeria',
+  		},
+		{
+		path: '/eventos/',
+    	url: 'eventos.html',
+    	name: 'eventos',
+  		}
+	],
+	dialog: {
+		title: 'Casa Di Vale App',
+		buttonOk: 'Aceptar',
+  	},
+	popup: {
+		closeOnEscape: true,
+		backdrop : false
+	},
+	sheet: {
+		closeOnEscape: true,
+	},
+	popover: {
+		closeOnEscape: true,
+	},
+	actions: {
+		closeOnEscape: true,
+	}
+});
+
+
+
+$$('#btnLogin').on('click', function (e) {
+	e.preventDefault();
+	
+	var $valid = $$('#form-login')[0].checkValidity();
+	if ($valid){
+        $$('#form-login').trigger('reset');	
+        app.loginScreen.close('#login');
+		
+		app.dialog.alert('¡ Welcome to Rungran Shop !');
+    }
+	
+});
+
+
+
+$$(document).on('click', '#btnAddCart', function (e) {
+	e.preventDefault();
+
+	
+	var notification = app.notification.create({
+      icon: '<i class="material-icons">check</i>',
+      title: 'Order',
+      titleRightText: '',
+      subtitle: '',
+      text: "Your order has been received.",
+      closeTimeout: 3000,
+    });
+    notification.open();
+	
+});
+
+
+
+
+
